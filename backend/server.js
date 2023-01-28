06575ac2afedffe13e61a4e402774fb998b78e58
Raphael FB <jvstraphael@gmail.com>
2023-01-29 00:11:23 +0200

    fixing issues

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send({
     people: '444adf'
   })
